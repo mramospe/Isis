f79a0a8f969d93e65b728a29613303cee95eabea
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2015-10-22 19:42:15 +0200

    Renamed Functors to Functions and removed class structure of them.

---
+++ General/include/Functions.h
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////////////////
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  General package      			                                 //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  AUTHOR : Miguel Ramos Pernas		                                 //
+//  e-mail: miguel.ramos.pernas@cern.ch		                                 //
+//                                                                               //
+//  Last Update: 22/10/2015			                                 //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  Description:				                                 //
+//						                                 //
+//  Implements different functions to perform calculations of physical and       //
+//  algebraic variables.                                                         //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+///////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef KIN_VARIABLES
+#define KIN_VARIABLES
+
+#include "LorentzVector.h"
+
+
+//_______________________________________________________________________________
+
+namespace General {
+
+  double CTAU( LorentzVector TLV, Vector3 OwnPV, Vector3 EndV );
+  double Det( Vector3 vec1, Vector3 vec2, Vector3 vec3 );
+  double Det( LorentzVector vec1, LorentzVector vec2, LorentzVector vec3 );
+  double IP( Vector3 vec, Vector3 pv, Vector3 sv );
+  double DOCA( LorentzVector vec1, LorentzVector vec2, Vector3 EndV1, Vector3 EndV2 );
+
+}
+
+#endif
--- General/source/Functions.cpp
+++ General/source/Functions.cpp
@@ -14,26 +14,23 @@
 //                                                                               //
 //  Description:				                                 //
 //						                                 //
-//  Implements different functions. All functions inherit from < Functor >       //
-//  class. If the class is created ( when no static method is used ) the         //
-//  < Calculate > method has to perform the calculations to get the desired      //
-//  variable. All the input and output variables are given to the class by       //
-//  reference, while the static method takes them by value.                      //
+//  Implements different functions to perform calculations of physical and       //
+//  algebraic variables.                                                         //
 //                                                                               //
 // ----------------------------------------------------------------------------- //
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-#include "Functors.h"
+#include "Functions.h"
 
 #include <cmath>
 
 
 //______________________________________________________________________________
-// Functor to calculate the half-life multiplied by the speed of light
-double General::CTAU::Eval( General::LorentzVector vec,
-			    General::Vector3       OwnPV,
-			    General::Vector3       EndV ) {
+// CTAU function
+double General::CTAU( General::LorentzVector vec,
+		      General::Vector3       OwnPV,
+		      General::Vector3       EndV ) {
 
   double
     beta2( vec.E()*vec.E()/( vec.P()*vec.P() ) ),
@@ -42,11 +39,11 @@ double General::CTAU::Eval( General::LorentzVector vec,
   return distance*std::sqrt( beta2 - 1 );
 }
 
-  //______________________________________________________________________________
-  // Functor to calculate the determinant of three Vector3 variables
-double General::Det::Eval( General::Vector3 vec1,
-			   General::Vector3 vec2,
-			   General::Vector3 vec3 ) {
+//______________________________________________________________________________
+// Calculates the determinant of three Vector3
+double General::Det( General::Vector3 vec1,
+		     General::Vector3 vec2,
+		     General::Vector3 vec3 ) {
    
   return 
     vec1.X()*( vec2.Y()*vec3.Z() - vec2.Z()*vec3.Y() ) - 
@@ -55,31 +52,42 @@ double General::Det::Eval( General::Vector3 vec1,
 }
 
 //______________________________________________________________________________
-// Functor to calculate the impact parameter given the momentum, the primary
-// and the secondary vertex positions
-double General::IP::Eval( General::Vector3 vec,
-			  General::Vector3 pv,
-			  General::Vector3 sv ) {
+// Calculates the determinant of three LorentzVector 
+double General::Det( General::LorentzVector vec1,
+		     General::LorentzVector vec2,
+		     General::LorentzVector vec3 ) {
+   
+  return 
+    vec1.Px()*( vec2.Py()*vec3.Pz() - vec2.Pz()*vec3.Py() ) - 
+    vec1.Py()*( vec2.Px()*vec3.Pz() - vec2.Pz()*vec3.Px() ) +
+    vec1.Pz()*( vec2.Px()*vec3.Py() - vec2.Py()*vec3.Px() );
+}
+
+//______________________________________________________________________________
+// Calculates the IP given the momentum and the primary and secondary vertex
+double General::IP( General::Vector3 vec,
+		    General::Vector3 pv,
+		    General::Vector3 sv ) {
 
   General::Vector3
     u( vec.Unitary() ),
     ipvec( pv - sv );
 
-  return ( ipvec - u.Dot( ipvec )*u ).Mod();
+  return ( ipvec - u.Dot( ipvec )*u ) .Mod();
 }
 
 //______________________________________________________________________________
-// Functor to calculate the distance of closest approach between two particles
-double General::DOCA::Eval( General::Vector3 vec1,
-			    General::Vector3 pos1,
-			    General::Vector3 vec2,
-			    General::Vector3 pos2 ) {
+// DOCA function (distance of closest approach)
+double General::DOCA( General::LorentzVector vec1,
+		      General::LorentzVector vec2,
+		      General::Vector3       EndV1,
+		      General::Vector3       EndV2 ) {
 
   General::Vector3
     u1( vec1.Unitary() ),
     u2( vec2.Unitary() );
 
-  double Vol( std::abs( Det::Eval( pos1 - pos2, u1, u2 ) ) );
+  double Vol( std::abs( Det( EndV1 - EndV2, u1, u2 ) ) );
 
   return Vol/( u1.Cross( u2 ) ).Mod();
 }
