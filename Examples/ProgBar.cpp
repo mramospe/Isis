7b7c97a383ac6671d9681c189cca31e5ef502b6d
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2015-10-23 12:10:16 +0200

    Added ProBar example

---
+++ Examples/ProgBar.cpp
@@ -0,0 +1,35 @@
+////////////////////////////
+//                        //
+//  Progress bar example  //
+//                        //
+////////////////////////////
+
+#include "ProgressBar.h"
+
+#include <iostream>
+
+int main() {
+
+  // Initializes the progress bar with all options
+  General::ProgressBar prbar("Bar=50:Percentage=1:Time:Date=Start-End");
+  
+  // Sets the color of the different options
+  prbar.SetColor( "Bar=32;41;1:Percentage=33;45;1:Time=33;3:StartDate=35;42;2:EndDate=31;45;2" );
+
+  std::cout << " Starting..." << std::endl;
+
+  // Number of iterations
+  size_t n = 100000000;
+
+  // Initializes the progress bar
+  prbar.Start( n );
+  for ( size_t i = 0; i < n; i++ )
+    prbar.Print( i );
+
+  // Prints the bar for the last time ( reaching automatically the 100% )
+  prbar.End();
+
+  std::cout << " Ending..." << std::endl;
+
+  return 0;
+}
