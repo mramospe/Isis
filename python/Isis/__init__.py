1ee9af51a6e32f704a59794233f565cf6e3fea66
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2015-12-01 19:59:24 +0100

    Modified __init__.py for the python modules. Now all classes can be accessed importing Isis, instead of having to specify the module.

--- python/Isis/__init__.py
+++ python/Isis/__init__.py
@@ -9,21 +9,29 @@
 #// ------------------------------------------------------- //
 #/////////////////////////////////////////////////////////////
 
-import os, sys
 
-# When importing all from Isis, there will be imported all the modules in the python folder
-path = os.path.abspath( __file__ )
-path = path[ :path.rfind( '/' ) ]
-__all__ = list( set( f[ :f.rfind( '.' ) ]
-                     for f in os.listdir( path )
-                     if ".py" in f and "__init__" not in f ) )
+import os, sys, pkgutil, inspect
+
 
 # If this __init__.py file is executed, it will import all Isis and ROOT modules. Since
 # ipython does not support the < import * > for the ROOT modules, if any argument is passed
-# when calling this file it will only do < import ROOT >.
+# when executing this file it will only do < import ROOT >.
 if __name__ == "__main__":
     from Isis import *
     if len( sys.argv ) > 1:
         import ROOT
     else:
         from ROOT import *
+# All the classes can be initialized from Isis directly, without having to specify the
+# module. However it is recommended to use the complete path of modules to avoid
+# confusions. Obviously, when importing all from Isis, there will be imported all the
+# modules in the python folder.
+else:
+    __all__ = []
+    for loader, name, is_pkg in pkgutil.walk_packages( __path__ ):
+        module = loader.find_module( name ).load_module( name )
+        for name, value in inspect.getmembers( module ):
+            if name.startswith( '__' ):
+                continue
+            globals()[ name ] = value
+            __all__.append( name )
