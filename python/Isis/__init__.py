901d3d589af58c8919f89f0969da48a3bf4eece0
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2015-12-01 14:33:16 +0100

    Modified __init__.py, and created isispy shell command. Now the Isis python folder can be called, loading all the submodules and the Root modules. The isispy command allows to initialize ipython and python with all these modules imported.

--- python/Isis/__init__.py
+++ python/Isis/__init__.py
@@ -1,11 +1,29 @@
+#/////////////////////////////////////////////////////////////
+#// ------------------------------------------------------- //
+#//                                                         //
+#//  AUTHOR: Miguel Ramos Pernas                            //
+#//  e-mail: miguel.ramos.pernas@cern.ch                    //
+#//                                                         //
+#//  Last update: 01/12/2015                                //
+#//                                                         //
+#// ------------------------------------------------------- //
+#/////////////////////////////////////////////////////////////
+
+import os, sys
+
 # When importing all from Isis, there will be imported all the modules in the python folder
-
-import os
-
 path = os.path.abspath( __file__ )
 path = path[ :path.rfind( '/' ) ]
-
 __all__ = list( set( f[ :f.rfind( '.' ) ]
                      for f in os.listdir( path )
                      if ".py" in f and "__init__" not in f ) )
 
+# If this __init__.py file is executed, it will import all Isis and ROOT modules. Since
+# ipython does not support the < import * > for the ROOT modules, if any argument is passed
+# when calling this file it will only do < import ROOT >.
+if __name__ == "__main__":
+    from Isis import *
+    if len( sys.argv ) > 1:
+        import ROOT
+    else:
+        from ROOT import *
