adac09afed8fc85b0a5858ef7379cabd6a8cd44b
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2016-01-02 17:55:35 +0100

    Added LoopArray class to allow the creation of undefined concatenations of loops

---
+++ General/include/LoopArray.h
@@ -0,0 +1,104 @@
+///////////////////////////////////////////////////////////////////////////////////
+//                                                                               //
+//  General package                                                              //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  AUTHOR: Miguel Ramos Pernas                                                  //
+//  e-mail: miguel.ramos.pernas@cern.ch                                          //
+//                                                                               //
+//  Last update: 02/01/2016                                                      //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  Description:                                                                 //
+//                                                                               //
+//  Implements the class to generate an array of indices to perform loops. This  //
+//  class is useful when one defines a general method to perform a number of     //
+//  loops ( usually "for" statements ) that can vary depending on different      //
+//  external conditions. This class behaves in the same way as a concatenation   //
+//  of "for" statements. Using the < ++ > operator, it will add units to the     //
+//  last index till the maximum is reached, when it is reset and a new unit is   //
+//  added to the previous one, on a recursively process. The maximum number of   //
+//  loops is obtained using the < GetNloops > method.                            //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+///////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef LOOP_ARRAY
+#define LOOP_ARRAY
+
+#include <iostream>
+#include <vector>
+
+
+//_______________________________________________________________________________
+
+namespace General {
+
+  class LoopArray {
+
+  public:
+
+    // Constructors
+    LoopArray();
+    LoopArray( const LoopArray &other );
+    LoopArray( size_t size, size_t max );
+    LoopArray( size_t size, size_t ( *func )( size_t ) );
+
+    // Destructor
+    ~LoopArray();
+
+    // Methods
+    inline void         AddIndex( size_t max );
+    inline const size_t GetNindices() const;
+    inline const size_t GetNloops() const;
+
+    // Iterative methods
+    inline std::vector<std::pair<size_t, size_t> >::const_iterator cBegin();
+    inline std::vector<std::pair<size_t, size_t> >::const_iterator cEnd();
+
+    // Operators
+    LoopArray&  operator ++ ();
+    LoopArray   operator ++ ( int );
+    const size_t operator [] ( size_t index ) const;
+
+  protected:
+
+    // Attributes
+    size_t                                  fNloops;
+    std::vector<std::pair<size_t, size_t> > fVector;
+  };
+
+  //______________________________
+  // INLINE METHODS
+
+  // Adds a new index variable to the array
+  inline void LoopArray::AddIndex( size_t max ) {
+    fVector.push_back( std::make_pair( 0, max ) );
+    fNloops *= max;
+  }
+  // Gets the number of indices stored in the array
+  inline const size_t LoopArray::GetNindices() const {
+    return fVector.size();
+  }
+  // Gets the number of loops that would be made using this array
+  inline const size_t LoopArray::GetNloops() const {
+  size_t res = 1;
+  for ( auto it = fVector.begin(); it != fVector.end(); it++ )
+    res *= it -> second;
+  return res;
+  }
+  // Returns the position to the first element of the vector of indices
+  inline std::vector<std::pair<size_t, size_t> >::const_iterator LoopArray::cBegin() {
+    return fVector.cbegin();
+  }
+  // Returns the position to the next-to-last element of the vector of indices
+  inline std::vector<std::pair<size_t, size_t> >::const_iterator LoopArray::cEnd() {
+    return fVector.cend();
+  }
+
+}
+
+#endif
---
+++ General/source/LoopArray.cpp
@@ -0,0 +1,103 @@
+///////////////////////////////////////////////////////////////////////////////////
+//                                                                               //
+//  General package                                                              //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  AUTHOR: Miguel Ramos Pernas                                                  //
+//  e-mail: miguel.ramos.pernas@cern.ch                                          //
+//                                                                               //
+//  Last update: 02/01/2016                                                      //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+//                                                                               //
+//  Description:                                                                 //
+//                                                                               //
+//  Implements the class to generate an array of indices to perform loops. This  //
+//  class is useful when one defines a general method to perform a number of     //
+//  loops ( usually "for" statements ) that can vary depending on different      //
+//  external conditions. This class behaves in the same way as a concatenation   //
+//  of "for" statements. Using the < ++ > operator, it will add units to the     //
+//  last index till the maximum is reached, when it is reset and a new unit is   //
+//  added to the previous one, on a recursively process. The maximum number of   //
+//  loops is obtained using the < GetNloops > method.                            //
+//                                                                               //
+// ----------------------------------------------------------------------------- //
+///////////////////////////////////////////////////////////////////////////////////
+
+
+#include "LoopArray.h"
+
+
+//_______________________________________________________________________________
+
+
+// -- CONSTRUCTORS AND DESTRUCTOR
+
+//_______________________________________________________________________________
+// Main constructor
+General::LoopArray::LoopArray() : fNloops( 1 ) { }
+
+//_______________________________________________________________________________
+// Copy constructor
+General::LoopArray::LoopArray( const LoopArray &other ) {
+  fNloops = other.fNloops;
+  fVector = other.fVector;
+}
+
+//_______________________________________________________________________________
+// Constructor given the sice of the array and the maximum loop value
+General::LoopArray::LoopArray( size_t size, size_t max ) :
+  fNloops( size*max ),
+  fVector( std::vector<std::pair<size_t, size_t> >( size, std::make_pair( 0, max ) ) ) { }
+
+//_______________________________________________________________________________
+// Constructor given the size of the array and a function that defines, for each
+// index, the maximum value of the loop
+General::LoopArray::LoopArray( size_t size, size_t ( *func )( size_t ) ) :
+  fNloops( 1 ),
+  fVector( std::vector<std::pair<size_t, size_t> >( size, std::make_pair( 0, 0 ) ) ) {
+  for ( size_t i = 0; i < size; i++ ) {
+    fVector[ i ].second = func( i );
+    fNloops *= fVector[ i ].second;
+  }
+}
+
+//_______________________________________________________________________________
+// Destructor
+General::LoopArray::~LoopArray() { }
+
+//_______________________________________________________________________________
+
+
+// -- OPERATORS
+
+//_______________________________________________________________________________
+// Moves one element forward in the array. This implies to add one unit to the
+// last index stored. If it reaches the associated maximum value it is reset,
+// and a unit is added to the next index ( doing this recursively ).
+General::LoopArray& General::LoopArray::operator ++ () {
+  fVector.back().first++;
+  auto it = fVector.rbegin();
+  while ( it -> first == it -> second ) {
+    it -> first = 0;
+    if ( it + 1 != fVector.rend() )
+      ( ++it ) -> first++;
+  }
+  return *this;
+}
+
+//_______________________________________________________________________________
+// Creates a copy of this class and adds a new unit to the current one. The old
+// class is returned.
+General::LoopArray General::LoopArray::operator ++ ( int ) {
+  General::LoopArray tmp( *this );
+  this -> operator ++ ();
+  return tmp;
+}
+
+//_______________________________________________________________________________
+// Returns the value of the index at position < index >
+const size_t General::LoopArray::operator [] ( size_t index ) const {
+  return fVector[ index ].first;
+}
