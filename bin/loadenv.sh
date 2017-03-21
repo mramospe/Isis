963d24f27c02d5d6a3febe60b4e7ac876ea443f1
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2017-03-21 14:20:27 +0100

    Environment variables are set by loadenv.sh given the output paths specified in CMakeLists.txt

--- bin/loadenv.sh
+++ bin/loadenv.sh
@@ -6,42 +6,37 @@
 #============================================
 
 # Setup message
-echo "*** Isis project ***"
-
-# Gets the path to this file
-currpath=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+echo "*** Isis project (v@PROJECT_VERSION@)***"
 
 # Exports the different folders of the project
-export ISIS=${currpath:0:(${#currpath}-4)}
-export ISIS_BIN=$currpath
-export ISIS_PYTHON=$ISIS/python
-export ISIS_SCRIPTS=$ISIS/scripts
+export ISIS_ROOT="@CMAKE_BINARY_DIR@"
+export ISIS_BIN="@INSTALL_BIN_DIR@"
+export ISIS_LIB="@INSTALL_LIB_DIR@"
+export ISIS_PYTHON="@INSTALL_PYTHON_DIR@"
 
 # Displays the path to the Isis folder
-echo "Loading source from: $ISIS"
+echo "Loading source from: $ISIS_ROOT"
 
-# Extends the path to bash scripts with this one
-if [[ $PATH != *$currpath* ]]; then
-    export PATH=$currpath:$PATH
-fi
-echo "Extended shell path from: $currpath"
+# Function to extend a given path if it has not been done yet
+function extend {
 
-# The library path is updated with the Isis libraries
-if [[ $LD_LIBRARY_PATH != *$ISIS/lib* ]]; then
-    export LD_LIBRARY_PATH=$ISIS/lib:$LD_LIBRARY_PATH
-fi
-echo "Exported c++ libraries from: $ISIS/lib"
-
-# Exports the python modules
-if [[ $PYTHONPATH != *$ISIS_PYTHON* ]]; then
-    export PYTHONPATH=$ISIS_PYTHON:$PYTHONPATH
-fi
-echo "Extended python path from: $ISIS_PYTHON"
+    if [[ ${!1} != *${!2}* ]]; then
+	export $1=${!2}:${!1}
+    fi
+}
 
 # Export cmake variables
-export CMAKE_PREFIX_PATH=$ISIS:$CMAKE_PREFIX_PATH
-echo "Extended CMake prefix path with: $ISIS"
+extend CMAKE_PREFIX_PATH ISIS_ROOT
+echo "Extended CMake prefix path with: $ISIS_ROOT"
 
-# Defines the alias to the scripts
-alias readCuts='python $ISIS_SCRIPTS/readCuts.py'
-echo "Defined alias for scripts at: $ISIS_SCRIPTS"
+# Extend the path for bash scripts
+extend PATH ISIS_BIN
+echo "Extended shell path with: $ISIS_BIN"
+
+# The library path is updated with the Isis libraries
+extend LD_LIBRARY_PATH ISIS_LIB
+echo "Exported c++ libraries with: $ISIS_LIB"
+
+# Export the path to the python modules
+extend PYTHONPATH ISIS_PYTHON
+echo "Extended python path with: $ISIS_PYTHON"
